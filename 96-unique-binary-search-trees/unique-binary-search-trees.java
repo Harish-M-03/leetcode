6449c04ad2909c21533d232bc84ee9c1a3f4308b
Harish-M-03 <harishmohankumar2004@gmail.com>
2026-02-19 13:46:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (12.76%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,10 +1,11 @@
 class Solution {
     public int numTrees(int n) {
-         int[] bt = {
-            1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796,
-            58786, 208012, 742900, 2674440, 9694845, 35357670,
-            129644790, 477638700, 1767263190
-        };
-        return bt[n-1];
+         int [] dp = new int[n+1];
+    dp[0]= 1;
+    dp[1] = 1;
+    for(int level = 2; level <=n; level++)
+        for(int root = 1; root<=level; root++)
+            dp[level] += dp[level-root]*dp[root-1];
+    return dp[n];
     }
 }

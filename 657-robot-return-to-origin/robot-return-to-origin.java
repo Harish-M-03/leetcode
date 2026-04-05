4afdd8c807df055bbd6b2e1bf220f3fefaca40c2
Harish-M-03 <harishmohankumar2004@gmail.com>
2026-04-05 11:00:41 +0530

    Time: 6 ms (49.49%) | Memory: 46 MB (48.28%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int vertical = 0, horizontal = 0;
-
-        for (char ch : moves.toCharArray()) {
-            if (ch == 'U') {
+        int vertical=0,horizontal=0;
+        for(char ch:moves.toCharArray()){
+            if(ch=='U'){
                 vertical++;
-            } else if (ch == 'D') {
+            }else if(ch=='D'){
                 vertical--;
-            } else if (ch == 'L') {
+            }else if(ch=='L'){
                 horizontal--;
-            } else if (ch == 'R') {
+            }else if(ch=='R'){
                 horizontal++;
             }
         }
-
-        return vertical == 0 && horizontal == 0;
+        return horizontal==0&&vertical==0;
     }
 }

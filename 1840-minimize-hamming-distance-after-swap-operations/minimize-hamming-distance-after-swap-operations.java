e26a07501fcb1afea609fa3a840488044e02b013
Harish-M-03 <harishmohankumar2004@gmail.com>
2026-04-21 19:11:11 +0530

    Time: 45 ms (81.72%) | Memory: 112.7 MB (81.72%) - LeetSync

---
+++ 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Solution {
+
+    class DSU {
+        int[] parent;
+
+        DSU(int n) {
+            parent = new int[n];
+            for (int i = 0; i < n; i++) parent[i] = i;
+        }
+
+        int find(int x) {
+            if (parent[x] != x)
+                parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        void union(int x, int y) {
+            parent[find(x)] = find(y);
+        }
+    }
+
+    public int minimumHammingDistance(int[] source, int[] target, int[][] allowedSwaps) {
+        int n = source.length;
+        DSU dsu = new DSU(n);
+
+      
+        for (int[] swap : allowedSwaps) {
+            dsu.union(swap[0], swap[1]);
+        }
+
+        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int parent = dsu.find(i);
+            map.putIfAbsent(parent, new HashMap<>());
+            Map<Integer, Integer> freq = map.get(parent);
+            freq.put(source[i], freq.getOrDefault(source[i], 0) + 1);
+        }
+
+       
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int parent = dsu.find(i);
+            Map<Integer, Integer> freq = map.get(parent);
+
+            if (freq.getOrDefault(target[i], 0) > 0) {
+                freq.put(target[i], freq.get(target[i]) - 1);
+            } else {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}

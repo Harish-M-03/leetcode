9d65c1dec17b3c0a1d52137e6bec1be32e9c3688
Harish-M-03 <harishmohankumar2004@gmail.com>
2026-05-03 16:29:43 +0530

    Time: 6 ms (10.10%) | Memory: 46.7 MB (29.81%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode temp = head;
+
+        while (temp != null) {
+            if (set.contains(temp)) {
+                return temp; 
+            }
+            set.add(temp);
+            temp = temp.next;
+        }
+
+        return null; // no cycle
+    }
+}

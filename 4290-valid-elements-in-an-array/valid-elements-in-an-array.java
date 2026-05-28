276ce346236c597b75b0c345228f56771ba736c9
Harish-M-03 <harishmohankumar2004@gmail.com>
2026-05-28 10:02:20 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (91.41%) - LeetSync

--- 4290-valid-elements-in-an-array/valid-elements-in-an-array.java
+++ 4290-valid-elements-in-an-array/valid-elements-in-an-array.java
@@ -1,34 +1,36 @@
 class Solution {
-    private boolean isValid(int left,int right,int[] nums,int element){
-        boolean isvalid=true;
-        for(int i=left;i<right;i++){
-            if(nums[i]>=element){
-                isvalid=false;
+    private boolean isValid(int n,int[] nums){
+        boolean left=true;
+        boolean right=true;
+        for(int i=0;i<n;i++){
+            if(nums[n]<=nums[i]){
+                left=false;
                 break;
             }
         }
-        return isvalid;
+        for(int i=n+1;i<nums.length;i++){
+            if(nums[n]<=nums[i]){
+                right=false;
+                break;
+            }
+        }
+        return left||right;
     }
     public List<Integer> findValidElements(int[] nums) {
-        List<Integer> res=new ArrayList<>();
-        if(nums.length<=2){
-            for(int num:nums){
-                res.add(num);
-            }
-        }
-        else{
-        List<Integer> arr=new ArrayList<>();
-        for(int i=1;i<nums.length-1;i++){
-            if(isValid(0,i,nums,nums[i])||isValid(i+1,nums.length,nums,nums[i])){
+      List<Integer> arr=new ArrayList<>();
+       if(nums.length<2){
+            for(int i=0;i<nums.length;i++){
                 arr.add(nums[i]);
             }
-        }
-        res.add(nums[0]);
-        for(int num:arr){
-            res.add(num);
-        }
-        res.add(nums[nums.length-1]);
-        }
-        return res;
+       }else{
+       arr.add(nums[0]);
+       for(int i=1;i<nums.length-1;i++){
+            if(isValid(i,nums)){
+                arr.add(nums[i]);
+            }
+       }
+       arr.add(nums[nums.length-1]);
+       }
+       return arr;
     }
 }

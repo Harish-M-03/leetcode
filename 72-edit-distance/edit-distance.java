baac21c6dd7e7870175f36176eb1aad2d79b0226
Harish-M-03 <harishmohankumar2004@gmail.com>
2026-06-20 23:11:34 +0530

    Time: 5 ms (69.18%) | Memory: 47.1 MB (52.19%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,33 +1,23 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int[][] cache = new int[word1.length() + 1][word2.length() + 1];
-
-        for (int i = 0; i <= word1.length(); i++) {
-            for (int j = 0; j <= word2.length(); j++) {
-                cache[i][j] = -1;
+        int n=word1.length();
+        int m=word2.length();
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+                }
             }
         }
-
-        return dp(word1, word2, word1.length(), word2.length(), cache);      
+        return dp[n][m];
     }
-
-    private int dp(String word1, String word2, int i, int j, int[][] cache) {
-        if (i == 0) return j;
-        if (j == 0) return i;
-        if (cache[i][j] != -1) return cache[i][j];
-
-        if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-            cache[i][j] = dp(word1, word2, i - 1, j - 1, cache);
-        } else {
-            cache[i][j] = 1 + Math.min(
-                dp(word1, word2, i - 1, j, cache),        // Delete
-                Math.min(
-                    dp(word1, word2, i, j - 1, cache),    // Insert
-                    dp(word1, word2, i - 1, j - 1, cache) // Replace
-                )
-            );
-        }
-
-        return cache[i][j];
-    }   
 }

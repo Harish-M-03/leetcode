fa8e5d6dd1c7486c391b2e233f514dfd2b08ed70
Harish-M-03 <harishmohankumar2004@gmail.com>
2026-06-22 10:41:13 +0530

    Time: 2 ms (96.67%) | Memory: 43.3 MB (79.23%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        String str="balloon";
+        int[] freq=new int[26];
+        for(char ch:text.toCharArray()){
+            freq[ch-'a']++;
+        }
+        int count=0;
+        while(true){
+            boolean isValid=true;
+            int cnt=0;
+            for(char ch:str.toCharArray()){
+                if(freq[ch-'a']>=1){
+                    freq[ch-'a']--;
+                    cnt++;
+                }else{
+                    isValid=false;
+                    break;
+                }
+            }
+            if(!isValid&&cnt!=7){
+                break;
+            }else{
+                count+=1;
+            }
+        }
+        return count;
+    }
+}

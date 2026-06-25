7935a832fdeb4cf10ba0d5b69394cb0af4b1d68f
Harish-M-03 <harishmohankumar2004@gmail.com>
2026-06-25 10:42:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (41.88%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,13 +1,39 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        for(int i=1;i<nums.length;i++){
-            int j=i-1;
-            int key=nums[i];
-            while(j>=0&&nums[j]>=key){
-                nums[j+1]=nums[j];
-                j=j-1;
-            }
-            nums[j+1]=key;
+    private void merge(int[] nums,int left,int right){
+        if(left<right){
+            int mid=left+(right-left)/2;
+            merge(nums,left,mid);
+            merge(nums,mid+1,right);
+            mergesort(nums,left,mid,right);
         }
+    }
+    private void mergesort(int[] nums,int left,int mid,int right){
+        int n1=mid-left+1;
+        int n2=right-mid;
+        int[] L=new int[n1];
+        int[] R=new int[n2];
+        for(int i=0;i<n1;i++){
+            L[i]=nums[left+i];
+        }
+        for(int i=0;i<n2;i++){
+            R[i]=nums[mid+i+1];
+        }
+        int j=0,l=0,k=left;
+        while(j<n1&&l<n2){
+            if(L[j]<=R[l]){
+                nums[k++]=L[j++];
+            }else{
+                nums[k++]=R[l++];
+            }
+        }
+        while(j<n1){
+            nums[k++]=L[j++];
+        }
+        while(l<n2){
+            nums[k++]=R[l++];
+        }
+    }
+    public void sortColors(int[] nums) {
+        merge(nums,0,nums.length-1);
     }
 }

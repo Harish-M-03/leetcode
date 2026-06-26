d981915aaa78b8e669a5b50331dc2d3de6252695
Harish-M-03 <harishmohankumar2004@gmail.com>
2026-06-26 14:02:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (12.09%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -9,53 +9,32 @@
  * }
  */
 class Solution {
-    private ListNode reverseList(ListNode head){
-        Stack<Integer> stack=new Stack<>();
-        ListNode temp=head;
-        while(temp!=null){
-            stack.push(temp.val);
-            temp=temp.next;
-        }
+    public ListNode reverseBetween(ListNode head, int left, int right) {
         ListNode newHead=new ListNode(-1);
         ListNode ptr=newHead;
-        while(!stack.isEmpty()){
-            ptr.next=new ListNode(stack.pop());
-            ptr=ptr.next;
-        }
-        return newHead.next; 
-    } 
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode list1=new ListNode(-1);
-        ListNode ptr=list1;
-        int count=1;
+        Stack<Integer> stack=new Stack<>();
+        int cnt=0;
         ListNode temp=head;
         while(temp!=null){
-            if(count>=left&&count<=right){
+            cnt++;
+            if(cnt>=left&&cnt<=right){
+                stack.push(temp.val);
+            }
+            temp=temp.next;
+        }
+        temp=head;
+        cnt=0;
+        while(temp!=null){
+            cnt++;
+            if(cnt>=left&&cnt<=right){
+                ptr.next=new ListNode(stack.pop());
+                ptr=ptr.next;
+            }else{
                 ptr.next=new ListNode(temp.val);
                 ptr=ptr.next;
             }
-            count++;
             temp=temp.next;
         }
-        ListNode sample=list1.next;
-        ListNode reverse=reverseList(sample);
-        ListNode list2=new ListNode(-1);
-        ListNode ptr2=list2;
-        temp=head;
-        int fincount=1;
-        while(temp!=null){
-            if(fincount>=left&&fincount<=right){
-                ptr2.next=new ListNode(reverse.val);
-                reverse=reverse.next;
-                ptr2=ptr2.next;
-                temp=temp.next;
-            }else{
-                ptr2.next=new ListNode(temp.val);
-                ptr2=ptr2.next;
-                temp=temp.next;
-            }
-            fincount++;
-        }
-        return list2.next;
+        return newHead.next;
     }
 }

75f3ecfc59db252108941040076ffb7b9c8dda61
Harish-M-03 <harishmohankumar2004@gmail.com>
2026-06-27 14:01:19 +0530

    Time: 61 ms (8.04%) | Memory: 99.5 MB (5.11%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -1,15 +1,24 @@
 class Solution {
+    private int[] merge(int[] num1,int[] num2){
+        int[] result=new int[2];
+        result[0]=Math.max(num1[0],num2[0]);
+        result[1]=Math.min(num1[1],num2[1]);
+        return result;
+    }
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points,(a,b)->Integer.compare(a[1],b[1]));
-        int arrow = 1;
-        int last = points[0][1];
+        Arrays.sort(points,(a,b)->Integer.compare(a[0],b[0]));
+        int[][] result=new int[points.length][2];
+        int k=0;
+        result[k]=points[0];
         for(int i=1;i<points.length;i++){
-            if(points[i][0]>last){
-                arrow++;
-                last=points[i][1];
+            int[] num1=result[k];
+            int[] num2=points[i];
+            if(num1[1]<num2[0]){
+                result[++k]=num2;
+            }else{
+                result[k]=merge(num1,num2);
             }
         }
-        return arrow;
-
+        return k+1; 
     }
 }

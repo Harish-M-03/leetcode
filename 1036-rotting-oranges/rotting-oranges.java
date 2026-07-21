d9ee453c180d74cc58d047cd19f906d1594db04c
Harish-M-03 <harishmohankumar2004@gmail.com>
2026-07-21 20:05:42 +0530

    Time: 2 ms (91.02%) | Memory: 44.4 MB (25.00%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,63 +1,37 @@
 class Solution {
-    static class selva{
-        int i;
-        int j;
-        int time;
-        selva(int i, int j, int time){
-            this.i=i;
-            this.j=j;
-            this.time=time;
-        }
-    }
-
     public int orangesRotting(int[][] grid) {
-
-        int time=0;
-        int m=grid.length;
-        int n=grid[0].length;
-
-        Queue<selva> queue=new LinkedList<>();
-
-        // Step 1: push all rotten oranges into queue
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        int fresh=0,minutes=0;
+        Queue<int[]> queue=new LinkedList<>();
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==2){
-                    queue.add(new selva(i,j,0));
+                    queue.offer(new int[]{i,j});
+                }else if(grid[i][j]==1){
+                    fresh++;
                 }
             }
         }
-
-        // directions: left, right, up, down
-        int dir[][]={
-            {0,-1},
-            {0,1},
-            {-1,0},
-            {1,0}
-        };
-
-        // Step 2: BFS spreading
+        int[][] dir={{-1,0},{1,0},{0,-1},{0,1}};
         while(!queue.isEmpty()){
-            selva temp=queue.poll();
-
-            for(int k=0;k<4;k++){
-                int ii=temp.i+dir[k][0];
-                int jj=temp.j+dir[k][1];
-
-                if(ii>=0 && ii<m && jj>=0 && jj<n && grid[ii][jj]==1){
-                    grid[ii][jj]=2;
-                    queue.add(new selva(ii,jj,temp.time+1));
-                    time=Math.max(time,temp.time+1);
+            boolean isRotten=false;
+            int size=queue.size();
+            while(size-->0){
+                int[] cur=queue.poll();
+                for(int[] d:dir){
+                    int nr=d[0]+cur[0];
+                    int nc=d[1]+cur[1];
+                    if(nr>=0&&nr<grid.length&&nc>=0&&nc<grid[0].length&&grid[nr][nc]==1){
+                        grid[nr][nc]=2;
+                        queue.offer(new int[]{nr,nc});
+                        fresh--;
+                        isRotten=true;
+                    }
                 }
             }
-        }
-
-        // Step 3: check if any fresh orange remains
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1) return -1;
+            if(isRotten){
+                minutes++;
             }
         }
-
-        return time;
+        return fresh==0?minutes:-1;
     }
 }

4d1cd5387837dc0614bbc6d528f12f7d92bc840c
Harish-M-03 <harishmohankumar2004@gmail.com>
2026-07-27 16:00:52 +0530

    Time: 1 ms (99.98%) | Memory: 45.6 MB (39.67%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -14,22 +14,25 @@
  * }
  */
 class Solution {
-    List<List<Integer>> result=new ArrayList<>();
-    private void pathsum(TreeNode root,int target,int sum,ArrayList<Integer> ar){
+    List<List<Integer>> arr=new ArrayList<>();
+    private void pathsum(TreeNode root,int sum,int target,ArrayList<Integer> ar){
         if(root==null){
             return;
         }
         sum+=root.val;
         ar.add(root.val);
         if(root.left==null&&root.right==null&&target==sum){
-            result.add(new ArrayList<>(ar));
+            arr.add(new ArrayList<>(ar));
         }
-        pathsum(root.left,target,sum,ar);
-        pathsum(root.right,target,sum,ar);
-        ar.remove(ar.size()-1); 
+        pathsum(root.left,sum,target,ar);
+        pathsum(root.right,sum,target,ar);
+        ar.remove(ar.size()-1);
     }
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        pathsum(root,targetSum,0,new ArrayList<>());
-        return result;
+        if(root==null){
+            return arr;
+        }
+        pathsum(root,0,targetSum,new ArrayList<>());
+        return arr;
     }
 }

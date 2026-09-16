a696f4ac6286c7d2eb5a19fd414eb48f22debfbb
Harish-M-03 <harishmohankumar2004@gmail.com>
2026-09-16 20:48:58 +0530

    Time: 64 ms (5.73%) | Memory: 99.6 MB (5.06%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -1,24 +1,20 @@
 class Solution {
-    private int[] merge(int[] num1,int[] num2){
-        int[] result=new int[2];
-        result[0]=Math.max(num1[0],num2[0]);
-        result[1]=Math.min(num1[1],num2[1]);
-        return result;
-    }
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points,(a,b)->Integer.compare(a[0],b[0]));
-        int[][] result=new int[points.length][2];
-        int k=0;
-        result[k]=points[0];
-        for(int i=1;i<points.length;i++){
-            int[] num1=result[k];
-            int[] num2=points[i];
-            if(num1[1]<num2[0]){
-                result[++k]=num2;
+       Arrays.sort(points,(a,b)->Integer.compare(a[0],b[0]));
+       int[][] result=new int[points.length][2];
+       int k=0;
+       result[k]=points[0];
+       for(int i=1;i<points.length;i++){
+            int[] nums1=result[k];
+            int[] nums2=points[i];
+            if(nums1[1]<nums2[0]){
+                result[++k]=nums2;
             }else{
-                result[k]=merge(num1,num2);
+                int n1=Math.max(nums1[0],nums2[0]);
+                int n2=Math.min(nums1[1],nums2[1]);
+                result[k]=new int[]{n1,n2};
             }
-        }
-        return k+1; 
+       }
+       return k+1;
     }
 }
